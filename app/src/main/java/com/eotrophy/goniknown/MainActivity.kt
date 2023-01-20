7ff502c4cba84a1077624fbb7638888465d10fed
Taoist <eomiatrop@gmail.com>
2023-01-21 03:06:16 +0400

    Drawer was added

--- app/src/main/java/com/eotrophy/goniknown/MainActivity.kt
+++ app/src/main/java/com/eotrophy/goniknown/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +26,16 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.
         findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
+
+        val drawerNaviView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(drawerNaviView, navController)
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_bar)
         bottomNavigationView.setupWithNavController(navController)
 
         val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)
         val appBarConfiguration = builder.build()
         toolbar.setupWithNavController(navController, appBarConfiguration)
     }
